035641c3c2fff261b027da28745c6aacf0480913
himadri2110 <18it.himadri.shah@gmail.com>
2022-01-04 15:45:35 +0530

    feat: generate random meme image

---
+++ src/Components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <header className="header">Meme Generator</header>;
+};
+
+export default Header;
---
+++ src/Components/MemeGenerator.js
@@ -0,0 +1,71 @@
+import React from "react";
+
+class MemeGenerator extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      topText: "",
+      bottomText: "",
+      randomImg: "http://i.imgflip.com/1bij.jpg",
+      allMemeImgs: [],
+    };
+
+    this.changeHandler = this.changeHandler.bind(this);
+    this.submitHandler = this.submitHandler.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({ allMemeImgs: response.data.memes });
+        console.log(this.state.allMemeImgs);
+      });
+  }
+
+  changeHandler(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  submitHandler(event) {
+    event.preventDefault();
+
+    const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length);
+    const memeImg = this.state.allMemeImgs[randNum].url;
+    this.setState({ randomImg: memeImg });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.submitHandler}>
+          <input
+            type="text"
+            name="topText"
+            placeholder="Top Text"
+            value={this.state.topText}
+            onChange={this.changeHandler}
+          />
+
+          <input
+            type="text"
+            name="bottomText"
+            placeholder="Bottom Text"
+            value={this.state.bottomText}
+            onChange={this.changeHandler}
+          />
+
+          <button>Generate Meme</button>
+        </form>
+
+        <div className="meme">
+          <img src={this.state.randomImg} alt="Meme" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MemeGenerator;
