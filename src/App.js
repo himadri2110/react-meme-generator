40fc3ac64e97b48665cc75222396874586cebb50
himadri2110 <18it.himadri.shah@gmail.com>
2022-01-04 19:16:40 +0530

    feat: Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./Components/Header";
 import MemeGenerator from "./Components/MemeGenerator";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MemeGenerator />
+      <Footer />
     </div>
   );
 }
